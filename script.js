9aacccc6df263c37423d288a6f2530d0a741e045
Margaret Godowns <margaretgodowns@gmail.com>
2014-08-04 22:51:00 -0400

    more jQuery events

--- script.js
+++ script.js
@@ -14,18 +14,13 @@ $(function(){
   })
 });
 
-$(function(){
-  $(".squareThree").slideUp();
-  $(".squareThree").on('click', function(){
-    $(this).slideUp(25);
-  })
+$(".squareThree").on("click", function(){
+  $(this).slideUp(500);
 });
 
-$(function(){
-  $(".squareFour").fadeOut();
-  $(".squareFour").on('click', function(){
-    $(this).fadeOut(1000);
-  })
+
+$(".squareFour").on("click", function(){
+  $(this).fadeOut(1000)
 });
 
 $(function(){
@@ -37,3 +32,53 @@ $(function(){
     );
   });
 });
+
+$(".squareSix").on('click',function(){
+  $(this).animate({
+    height: '+=600'
+  }, 200, function(){
+    $(this).remove();
+  });
+});
+
+$(".squareSeven").click(function(){
+  $(this).css("height","600px")
+  $(this).css("borderBottomWidth", "50px")
+  $(this).css("borderTopWidth", "50px")
+});
+
+$("#square8").mouseover(function(event){
+  $(this).addClass("jumble");
+});
+
+$("#square8").mouseout(function(event){
+  $(this).removeClass("jumble");
+});
+
+$("#square9").dblclick(function(event){
+  $(this).addClass("zoom");
+});
+
+// $("#square9").dblclick(function(event){
+//   $(this).removeClass("zoom");
+// });
+
+$(".square10").mouseover(function(event){
+  $(this).addClass("jumble");
+  $(this).css("borderWidth", "35px")
+});
+
+$(".square10").mouseout(function(event){
+  $(this).removeClass("jumble");
+});
+
+$(".square11").click(function(event){
+  $(this).addClass("zoom");
+  $(this).css("border-radius", "10%")
+  $(this).css("margin-top","200px")
+  $(this).css("background-color","#f49ea7")
+});
+
+// $(".square11").click(function(event){
+//   $(this).removeClass("zoom");
+// });
